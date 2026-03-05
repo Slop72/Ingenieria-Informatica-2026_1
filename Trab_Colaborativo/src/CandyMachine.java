e054246a1def135133b28ec4a2bb9762f11f4f0d
Sebas <Sebas@10.16.26.124>
2026-03-05 08:58:09 -0600

    Es el Cnady machine con el Emi

---
+++ Trab_Colaborativo/src/CandyMachine.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class CandyMachine 
+{
+	static Scanner console = new Scanner(System.in);
+	public static void main(String[] args)
+	{
+	CashRegister cashRegister = new CashRegister(); //Step 1
+	Dispenser candy = new Dispenser(100, 50); //Step 2
+	Dispenser chips = new Dispenser(100, 65); //Step 2
+	Dispenser gum = new Dispenser(75, 45); //Step 2
+	Dispenser cookies = new Dispenser(100, 85); //Step 2
+	int choice; //variable to hold the selection //Step 3
+	showSelection(); //Step 4
+	choice = console.nextInt(); //Step 5
+		while (choice != 9) //Step 6
+		{
+		switch (choice) //Step 6a
+		{
+		case 1:
+			sellProduct(candy, cashRegister);
+			break;
+		case 2:
+			sellProduct(chips, cashRegister);
+			break;
+		case 3:
+			sellProduct(gum, cashRegister);
+			break;
+		case 4:
+			sellProduct(cookies, cashRegister);
+			break;
+		default:
+			System.out.println("Invalid Selection");
+			}
+			showSelection();
+			choice = console.nextInt();
+		}
+	}
+	//Place the definition of the method showSelection as
+	//given above here.
+	//Place the definition of the method sellProduct as
+	//given above here.
+}
